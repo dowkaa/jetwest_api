7b096482d38c28ea03c8d2ef415e4c47ce5fdd9a
mac <mac@macs-MacBook-Pro-2.local>
2022-12-31 17:23:41 +0100

    update

--- migrations/20221228062853-update_cargos.js
+++ migrations/20221228062853-update_cargos.js
@@ -5,31 +5,31 @@ module.exports = {
     return Promise.all([
       queryInterface.addColumn("cargos", "airworthiness_cert_status", {
         allowNull: true,
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
       }),
       queryInterface.addColumn("cargos", "noise_cert_status", {
         allowNull: true,
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
       }),
       queryInterface.addColumn("cargos", "insurance_cert_status", {
         allowNull: true,
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
       }),
       queryInterface.addColumn("cargos", "registration_cert_status", {
         allowNull: true,
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
       }),
       queryInterface.addColumn("cargos", "maintenance_program_status", {
         allowNull: true,
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
       }),
       queryInterface.addColumn("cargos", "mmel_status", {
         allowNull: true,
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
       }),
       queryInterface.addColumn("cargos", "ops_manual_status", {
         allowNull: true,
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
       }),
       queryInterface.addColumn("cargos", "note", {
         allowNull: true,
