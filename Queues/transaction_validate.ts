a1b43d33906e9cd836088f514cc99ac56b3ad586
mac <mac@macs-MacBook-Pro-2.local>
2023-01-17 18:10:30 +0100

    update

--- Queues/transaction_validate.ts
+++ Queues/transaction_validate.ts
@@ -87,32 +87,38 @@ const addJob = async (data: any) => {
             : shipment.weigth,
       });
 
-      await db.dbs.Transactions.create({
-        user_id: data.customer_id,
-        amount: amount,
+      let checkT = await db.dbs.Transactions.findOne({
         reference: data.reference,
-        departure_date: shipment.departure_date,
-        arrival_date: shipment.arrival_date,
-        shipment_no: data.shipment_num,
-        weight:
-          parseInt(shipment.volumetric_weight) > parseInt(shipment.weigth)
-            ? shipment.volumetric_weight
-            : shipment.weigth,
-        pricePerkeg: shipment.ratePerKg,
-        no_of_bags: shipment.no_of_bags,
-        type: "credit",
-        method: "paystack",
-        description: `Payment for shipment with no ${shipment.shipment_no}`,
-        status: "success",
       });
 
-      let checker = await db.dbs.PaystackStarter.findOne({
-        where: { reference: data.reference },
-      });
+      if (!checkT) {
+        await db.dbs.Transactions.create({
+          user_id: data.customer_id,
+          amount: amount,
+          reference: data.reference,
+          departure_date: shipment.departure_date,
+          arrival_date: shipment.arrival_date,
+          shipment_no: data.shipment_num,
+          weight:
+            parseInt(shipment.volumetric_weight) > parseInt(shipment.weigth)
+              ? shipment.volumetric_weight
+              : shipment.weigth,
+          pricePerkeg: shipment.ratePerKg,
+          no_of_bags: shipment.no_of_bags,
+          type: "credit",
+          method: "paystack",
+          description: `Payment for shipment with no ${shipment.shipment_no}`,
+          status: "success",
+        });
 
-      if (checker) {
-        checker.status = "success";
-        await checker.save();
+        let checker = await db.dbs.PaystackStarter.findOne({
+          where: { reference: data.reference },
+        });
+
+        if (checker) {
+          checker.status = "success";
+          await checker.save();
+        }
       }
 
       // const option = {
