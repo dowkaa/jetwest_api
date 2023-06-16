3732856fc22a368e49703311b93a8f9acfff6e4f
Abel kalu <abelkalu@Abels-MacBook-Pro.local>
2023-06-16 08:24:53 +0100

    updates

--- utils/server.ts
+++ utils/server.ts
@@ -12,7 +12,7 @@ function createServer() {
 
   const apiLimiter = packages.rateLimit({
     windowMs: 1000, // 15 minutes
-    max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     message: "Api call error, too many requests",
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
